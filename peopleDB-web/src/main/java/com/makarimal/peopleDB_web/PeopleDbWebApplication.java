c200f2100388d6cb6e4c8b492d14fb61ffcd5943
massamba08 <samndodia@gmail.com>
2024-10-09 11:25:24 +0200

    first commit

---
+++ peopleDB-web/src/main/java/com/makarimal/peopleDB_web/PeopleDbWebApplication.java
@@ -0,0 +1,13 @@
+package com.makarimal.peopleDB_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeopleDbWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeopleDbWebApplication.class, args);
+	}
+
+}
---
+++ peopleDB-web/src/test/java/com/makarimal/peopleDB_web/PeopleDbWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.makarimal.peopleDB_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeopleDbWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
