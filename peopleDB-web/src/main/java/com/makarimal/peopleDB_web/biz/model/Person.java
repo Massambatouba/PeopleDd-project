0ddfc1f9697ced3c230f07f7b9fa638b7322b18f
massamba08 <samndodia@gmail.com>
2024-10-09 17:21:49 +0200

    introdicing bootstrap and formatting dates and salary

--- peopleDB-web/src/main/java/com/makarimal/peopleDB_web/biz/model/Person.java
+++ peopleDB-web/src/main/java/com/makarimal/peopleDB_web/biz/model/Person.java
@@ -6,15 +6,27 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
+import java.text.NumberFormat;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class Person {
     private Long id;
-    private String firstname;
+    private String firstName;
     private String lastName;
     private LocalDate dob;
     private BigDecimal salary;
+
+    public String getFormattedDOB() {
+        return DateTimeFormatter.ofPattern("MMMM dd, yyyy").format(dob);
+    }
+
+    public String getNumberFormatSALARY() {
+        return NumberFormat.getCurrencyInstance().format(salary);
+    }
+
+
 }
