15a2a0f417ceba6adf0290c0c3149087e32e0f77
massamba08 <samndodia@gmail.com>
2024-10-30 17:18:32 +0100

    validation of the data

--- peopleDB-web/src/main/java/com/makarimal/peopleDB_web/web/controller/peopleController.java
+++ peopleDB-web/src/main/java/com/makarimal/peopleDB_web/web/controller/peopleController.java
@@ -2,9 +2,11 @@ package com.makarimal.peopleDB_web.web.controller;
 
 import com.makarimal.peopleDB_web.biz.model.Person;
 import com.makarimal.peopleDB_web.data.PersonRepository;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,11 +18,11 @@ public class peopleController {
     @Autowired
     private PersonRepository personRepository;
 
-   /* public void PeopleController(PersonRepository personRepository) {
+    public void PeopleController(PersonRepository personRepository) {
         this.personRepository = personRepository;
     }
 
-    */
+
     @ModelAttribute("people")
     public Iterable<Person> getPeople() {
         return personRepository.findAll();
@@ -38,9 +40,12 @@ public class peopleController {
     }
 
     @PostMapping
-    public String savePerson(Person person) {
+    public String savePerson(@Valid Person person, Errors errors) {
         System.out.println(person);
-        personRepository.save(person);
-        return "redirect:/people";
+        if (!errors.hasErrors()) {
+            personRepository.save(person);
+            return "redirect:/people";
+        }
+        return "people";
     }
 }
